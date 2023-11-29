91653f7527ecc19d2fdcd31541fa43720da90dc7
RuslanMirov <ruslan777mirov@gmail.com>
2023-11-29 10:47:54 +0200

    Fix bug

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,4 @@
 export const NETID = 97
-export const DepositorERC20 = "0x701AF32fCfEE084E147dC5007a6DE3eCaFC8FD3b"
 export const DepositToken = "0x45A78D3473b38e500FF9Ede04ea151EF8999660B"
 export const DepositDecimals = 18
 export const NetIdMessage = "Please connect to BNB testnet, and reload page"
